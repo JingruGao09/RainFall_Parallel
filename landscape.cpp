57e952c4735c9455a8b53c9a7ac24aa4970fb926
JingruGao09 <jingru.gao.1996@gmail.com>
2019-11-17 14:52:59 -0500

    git

--- landscape.cpp
+++ landscape.cpp
@@ -1,42 +1,56 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <sstream>
-#include <vector>
 #include <string>
 #include <unordered_map>
-
+#include <vector>
 
 class Landscape {
 private:
   int dimention;
   int abs_rate;
   int drops;
-  std::string f_heights;
-  
-  
-  std::vector<std::vector<int> > land_matrix;
-  std::unordered_map<  > adjList;
-  std::vector<std::vector<int> > land_drops;
-  std::vector<std::vector<int> > update_matrix;
-  std::vector<std::vector<int> > land_absorb;
-  
+  std::string elevation_file;
+
+  std::vector<std::vector<int>> land_matrix;
+  std::unordered_map<> adjList;
+  std::vector<std::vector<int>> land_drops;
+  std::vector<std::vector<int>> update_matrix;
+  std::vector<std::vector<int>> land_absorb;
+
 public:
-  landscape(){}
-  landscaoe(int d, int abs, int d, int f):dimention(d),abs_rate(abs),drops(d),f_heights(f){
+  landscape() {}
+  landscaoe(int d, int abs, int d, int f)
+      : dimention(d), abs_rate(abs), drops(d), elevation_file(f) {
     getMatrix();
     getAdjList
   }
 
   void getMatrix() {
-    // read from the file and put into vector<vector> matrix  
+    ifstream in(elevation_file.c_str());
+    if (!in) {
+      cerr << "Cannot open the File : " << elevation_file << endl;
+      return;
+    }
+    string str;
+    int i = 0;
+    while (getline(in, str)) {
+      stringstream ss(str);
+      string drop;
+      while (getline(ss, drop, ' ')) {
+        rainFall[i].push_back(stoi(drop));
+      }
+      i++;
+    }
+    // read from the file and put into vector<vector> matrix
   }
 
   void getAdjList() {
-    //from matrix get all higher point's lowest neighbor
+    // from matrix get all higher point's lowest neighbor
   }
 
-  void getUpdates(){
-    //for each iteration, generate the update matrix for each point
+  void getUpdates() {
+    // for each iteration, generate the update matrix for each point
   }
 
   void updateDrops() {
@@ -47,9 +61,8 @@ public:
     // check whether has raining
     return drops != 0;
   }
-  
+
   bool isDry() {
-    //check whether all the drops are absorbed
+    // check whether all the drops are absorbed
   }
-  
 }
