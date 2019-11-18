1a44f3c235580feb7d1d71ed9b10555b7614d1be
JingruGao09 <jingru.gao.1996@gmail.com>
2019-11-17 20:11:09 -0500

    fixed segfault

--- landscape.cpp
+++ landscape.cpp
@@ -2,13 +2,11 @@
 
 using namespace std;
 
-
 Landscape::Landscape(int dim, int d, double abs, string f)
-  : dimention(dim), drops(d), abs_rate(abs), elevation_file(f),
-    land_drops(dim, vector<double>(dim,0)),
-    update_matrix(dim, vector<double>(dim,0)),
-    land_absorb(dim, vector<double>(dim,0))
-{
+    : dimention(dim), drops(d), abs_rate(abs), elevation_file(f), rainFall(dim),
+      land_drops(dim, vector<double>(dim, 0)),
+      update_matrix(dim, vector<double>(dim, 0)),
+      land_absorb(dim, vector<double>(dim, 0)) {
   getMatrix();
   getAdjList();
 }
@@ -16,23 +14,24 @@ Landscape::Landscape(int dim, int d, double abs, string f)
 Landscape::~Landscape() {}
 
 void Landscape::getMatrix() {
-    ifstream in(elevation_file.c_str());
-    if (!in) {
-      cerr << "Cannot open the File : " << elevation_file << endl;
-      return;
-    }
-    string str;
-    int i = 0;
-    while (getline(in, str)) {
-      stringstream ss(str);
-      string drop;
-      while (getline(ss, drop, ' ')) {
-        rainFall[i].push_back(stoi(drop));
-      }
-      i++;
-    }
-    // read from the file and put into vector<vector> matrix
+  ifstream in(elevation_file.c_str());
+  if (!in) {
+    cerr << "Cannot open the File : " << elevation_file << endl;
+    return;
   }
+  string str;
+  int i = 0;
+  while (getline(in, str)) {
+    stringstream ss(str);
+    string drop;
+    while (getline(ss, drop, ' ')) {
+      rainFall[i].push_back(stod(drop));
+    }
+    i++;
+  }
+
+  // read from the file and put into vector<vector> matrix
+}
 
 void Landscape::getAdjList() {
   // from matrix get all higher point's lowest neighbor
@@ -45,45 +44,45 @@ void Landscape::getAdjList() {
       int minRain = rainFall[i][j];
       int cnt = 0;
       for (int d = 0; d < 4; d++) {
-	int x = i + dirs[d][0];
-	int y = j + dirs[d][1];
-	if (x < 0 || x >= n || y < 0 || y >= n) {
-	  continue;
-	}
-	if (minRain < rainFall[x][y]) {
-	  continue;
-	}
-	if (minRain > rainFall[x][y]) {
-	  minRain = rainFall[x][y];
-	  auto p = make_pair(i, j);
-	  neighbors[p].clear();
-	  neighbors[p].push_back(d);
-	  cnt = 1;
-	  
-	} else if (minRain == rainFall[x][y]) {
-	  auto p = make_pair(i, j);
-	  neighbors[p].push_back(d);
-	  cnt++;
-	}
+        int x = i + dirs[d][0];
+        int y = j + dirs[d][1];
+        if (x < 0 || x >= n || y < 0 || y >= n) {
+          continue;
+        }
+        if (minRain < rainFall[x][y]) {
+          continue;
+        }
+        if (minRain > rainFall[x][y]) {
+          minRain = rainFall[x][y];
+          auto p = make_pair(i, j);
+          neighbors[p].clear();
+          neighbors[p].push_back(d);
+          cnt = 1;
+
+        } else if (minRain == rainFall[x][y]) {
+          auto p = make_pair(i, j);
+          neighbors[p].push_back(d);
+          cnt++;
+        }
       }
       if (cnt > 0 && minRain < rainFall[i][j]) {
-	cout << "Division, i " << i << ", j " << j << ", cnt " << cnt << endl;
-	
+        cout << "Division, i " << i << ", j " << j << ", cnt " << cnt << endl;
+
       } else {
-	cout << " no Division" << endl;
-	auto p = make_pair(i, j);
-	// neighbors[p].clear();
-	neighbors.erase(p);
-	// no division
+        cout << " no Division" << endl;
+        auto p = make_pair(i, j);
+        // neighbors[p].clear();
+        neighbors.erase(p);
+        // no division
       }
     }
   }
-  
+
   cout << "{{-1, 0}, {0, -1}, {1, 0}, {0, 1}}" << endl;
-  
+
   for (auto it : neighbors) {
     cout << "i: " << it.first.first << " j: " << it.first.second
-	 << " my neighbors: ";
+         << " my neighbors: ";
     for (auto i : it.second) {
       cout << i << " ";
     }
@@ -93,53 +92,52 @@ void Landscape::getAdjList() {
 
 void Landscape::getAbsorb() {
   // print std, final result
-  for ( size_t i = 0; i < dimention; i++ ) {
-    for ( size_t j = 0; j < dimention; j++ ) {  
-      cout << land_absorb[i][j] << " " << endl;
+  for (size_t i = 0; i < dimention; i++) {
+    for (size_t j = 0; j < dimention; j++) {
+      cout << land_absorb[i][j] << " ";
     }
-    cout << "\n" << endl;
- }
+    cout << endl;
+  }
 }
 
 void Landscape::getUpdates() {
   // for each iteration, generate the update matrix for each point
-  for ( size_t i = 0; i < dimention; i++ ) {
-    for ( size_t j = 0; j < dimention; j++ ) {
+  for (size_t i = 0; i < dimention; i++) {
+    for (size_t j = 0; j < dimention; j++) {
       // if raining, get rain
-      if ( isRaining() ) {
-	land_drops[i][j] += 1;
+      if (isRaining()) {
+        land_drops[i][j] += 1;
       }
-     // absorb by land -> update abs matrix 
-      if ( land_drops[i][j] > abs_rate ) {
-	land_drops[i][j] -= abs_rate;
-	land_absorb[i][j] += abs_rate;
-      }
-      else {
-	land_absorb[i][j] += land_drops[i][j];
-	land_drops[i][j] = 0;
-	wet_points--;
+      // absorb by land -> update abs matrix
+      if (land_drops[i][j] > abs_rate) {
+        land_drops[i][j] -= abs_rate;
+        land_absorb[i][j] += abs_rate;
+      } else {
+        land_absorb[i][j] += land_drops[i][j];
+        land_drops[i][j] = 0;
+        wet_points--;
       }
       // if remaining > 1 && has low neighbor, tickle to the min neighbor
       // update the update_matrix
-      if ( land_drops[i][j] >= 1) {
-	pair<int, int> curr = make_pair(i,j);
-	if ( neighbors.find(curr) != neighbors.end() ) {
-	  double trickle_drop = 1/neighbors[curr].size();
-	  const vector<int> & curr_neighbor = neighbors[curr]; 
-	  for ( size_t n = 0; n < curr_neighbor.size(); n++ ) {
-	    update_matrix[curr_neighbor[0]][curr_neighbor[1]] += trickle_drop;
-	  }
-	  land_drops[i][j] -= 1;
-	}// if has neighbor
-      }// if could trickle
-    }// j
-  }// i
+      if (land_drops[i][j] >= 1) {
+        pair<int, int> curr = make_pair(i, j);
+        if (neighbors.find(curr) != neighbors.end()) {
+          double trickle_drop = 1 / neighbors[curr].size();
+          const vector<int> &curr_neighbor = neighbors[curr];
+          for (size_t n = 0; n < curr_neighbor.size(); n++) {
+            update_matrix[curr_neighbor[0]][curr_neighbor[1]] += trickle_drop;
+          }
+          land_drops[i][j] -= 1;
+        } // if has neighbor
+      }   // if could trickle
+    }     // j
+  }       // i
 }
 
 void Landscape::updateDrops() {
   // update each point for land_drops from update_matrix;
-  for ( size_t i = 0; i < dimention; i++ ) {
-    for ( size_t j = 0; j < dimention; j++ ) {  
+  for (size_t i = 0; i < dimention; i++) {
+    for (size_t j = 0; j < dimention; j++) {
       land_drops[i][j] += update_matrix[i][j];
       update_matrix[i][j] = 0;
     }
@@ -156,34 +154,29 @@ bool Landscape::isDry() {
   return wet_points == 0;
 }
 
-
-
-void Landscape::printRainfall(){
-  for ( size_t i = 0; i < dimention; i++ ) {
-    for ( size_t j = 0; j < dimention; j++ ) {  
-      cout << rainFall[i][j] << " " << endl;
+void Landscape::printRainfall() {
+  for (size_t i = 0; i < dimention; i++) {
+    for (size_t j = 0; j < dimention; j++) {
+      cout << rainFall[i][j] << " ";
     }
-    cout << "\n" << endl;
+    cout << endl;
   }
 }
 
-
-void Landscape::printLandDrops(){
-  for ( size_t i = 0; i < dimention; i++ ) {
-    for ( size_t j = 0; j < dimention; j++ ) {  
-      cout << land_drops[i][j] << " " << endl;
+void Landscape::printLandDrops() {
+  for (size_t i = 0; i < dimention; i++) {
+    for (size_t j = 0; j < dimention; j++) {
+      cout << land_drops[i][j] << " ";
     }
-    cout << "\n" << endl;
+    cout << endl;
   }
 }
 
 void Landscape::printUpdateMatrix() {
-  for ( size_t i = 0; i < dimention; i++ ) {
-    for ( size_t j = 0; j < dimention; j++ ) {  
-      cout << update_matrix[i][j] << " " << endl;
+  for (size_t i = 0; i < dimention; i++) {
+    for (size_t j = 0; j < dimention; j++) {
+      cout << update_matrix[i][j] << " ";
     }
-    cout << "\n" << endl;
+    cout << endl;
   }
 }
-
-  
--- landscape.h
+++ landscape.h
@@ -1,74 +1,76 @@
 #ifndef __LANDSCAPE_H__
 #define __LANDSCAPE_H__
 
+#include <bits/stdc++.h>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <unordered_map>
-#include <vector>
 #include <utility>
-#include <cstdlib>
-#include <bits/stdc++.h>
+#include <vector>
 
 using namespace std;
 
-
 class Landscape {
-  private:
+private:
   size_t dimention;
   int drops;
   double abs_rate;
   string elevation_file;
-  int wet_points; // N * N 
+  int wet_points; // N * N
 
   struct hash_pair {
     size_t operator()(const pair<int, int> &p) const {
       auto hash1 = hash<int>{}(p.first);
       auto hash2 = hash<int>{}(p.second);
-      return hash1^hash2;
+      return hash1 ^ hash2;
     }
   };
-  
-  vector<vector<int> > rainFall; // matrix to record the height of each point
-  unordered_map<pair<int, int>, vector<int>, hash_pair> neighbors; // adjecency list to record each point's min neighbors
-  vector<vector<double> > land_drops; // matrix to record current drops value on each point
-  vector<vector<double> > update_matrix; // matrix to record the value that needed to update to each point
-  vector<vector<double> > land_absorb; // matrix to record the value of obsorbtion for each point
-  
- public:
+
+  vector<vector<double>> rainFall; // matrix to record the height of each point
+  unordered_map<pair<int, int>, vector<int>, hash_pair>
+      neighbors; // adjecency list to record each point's min neighbors
+  vector<vector<double>>
+      land_drops; // matrix to record current drops value on each point
+  vector<vector<double>> update_matrix; // matrix to record the value that
+                                        // needed to update to each point
+  vector<vector<double>>
+      land_absorb; // matrix to record the value of obsorbtion for each point
+
+public:
   Landscape();
   Landscape(int dim, int d, double abs, string f);
-  
+
   void getMatrix();
   // read from file and construct lancscape matrix
-  
+
   void getAdjList();
   // update min neighbors
 
   void getAbsorb();
-    // print std, final result
+  // print std, final result
 
   void getUpdates();
-    // for each iteration, generate the update matrix for each point
-  
+  // for each iteration, generate the update matrix for each point
+
   void updateDrops();
-    // update each point for land_drops from update_matrix
+  // update each point for land_drops from update_matrix
 
   bool isRaining();
-    // check whether has raining
+  // check whether has raining
 
   bool isDry();
-    // check whether all the drops are absorbed
+  // check whether all the drops are absorbed
 
   void printRainfall();
-  
+
   void printLandDrops();
 
   void printUpdateMatrix();
-  
-  ~Landscape();
 
+  ~Landscape();
 };
 
 #endif
